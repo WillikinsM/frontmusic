88920a0a8307d808566d971597d75141c71b05fc
WillikinsM <willikins98@gmail.com>
2022-07-04 17:02:01 -0300

    Inital configuration with Nextjs

---
+++ willmusic/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@chakra-ui/layout'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <Text>Hello World</Text>
+  )
+}
+
+export default Home
